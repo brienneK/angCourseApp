a60e9a707eedd818c7852a347f977f6a9985f1a8
Brienne Kordis <blkordis@gmail.com>
2024-12-06 17:35:03 -0500

    load user specific tasks

--- code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
+++ code-snapshots/02-essentials/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,13 +1,44 @@
 import { Component, Input, input } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({required: true}) userId!: string;
+  //userIid = input<string>();
+  @Input({required: true}) name!: string;
   // name = input<string>();
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title: 'Do stuff',
+      summary: 'Ya know, stuff',
+      dueDate: '2025-03-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'More stuff',
+      summary: 'More and more',
+      dueDate: '2025-07-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Bit more stuff',
+      summary: 'Adulting never ends',
+      dueDate: '2025-12-31'
+    },    
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
