94a237d7b531a4d692301b3dab1ee3bc29e20fb7
Brienne Kordis <blkordis@gmail.com>
2024-12-08 14:08:37 -0500

    calculate user input

--- code-snapshots/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
+++ code-snapshots/04-essentials-practice/01-starting-project/src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  calculate = output<{initialInvestment: number, duration: number, expectedReturn: number, annualInvestment: number}>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
@@ -16,10 +17,12 @@ export class UserInputComponent {
 
   onSubmit() {
     console.log('submitted');
-    console.log(this.enteredInitialInvestment);
-    console.log(this.enteredAnnualInvestment);
-    console.log(this.enteredExpectedReturn);
-    console.log(this.enteredDuration);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment
+    });
   }
 
 }
