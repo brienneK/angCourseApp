dfaaf845a4457b9db336897e59efc680f440bae1
Brienne Kordis <blkordis@gmail.com>
2024-12-08 16:52:25 -0500

    add investment service

--- code-snapshots/04-essentials-practice/01-starting-project/src/app/investment-results/investment-results.component.ts
+++ code-snapshots/04-essentials-practice/01-starting-project/src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InvestmentResults } from '../investment-results.model';
 import { CurrencyPipe } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -10,5 +11,9 @@ import { CurrencyPipe } from '@angular/common';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  results = input<InvestmentResults[]>();
+  private investmentService = inject(InvestmentService);
+
+  get results() {
+    return this.investmentService.resultsData;
+  }
 }
